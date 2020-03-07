c6c946fdb0aa90b75ab8bc46997cdc144b3b7227
Cesar Gimenes <crg@crg.eti.br>
2020-03-07 17:48:32 -0300

    add RGB parameters

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/png"
 	"os"
+	"strconv"
 
 	"pngtoansi/imgtoansi"
 
@@ -12,6 +13,7 @@ import (
 
 type config struct {
 	FileName string `cfg:"f" cfgRequired:"true"`
+	RGB      string `cfg:"rgb"`
 }
 
 func main() {
@@ -37,5 +39,21 @@ func main() {
 	}
 
 	p := imgtoansi.New()
+
+	if cfg.RGB != "" {
+		rgb, err := strconv.ParseUint(cfg.RGB, 16, 64)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		r := uint8(rgb >> 16)
+		g := uint8(rgb >> 8)
+		b := uint8(rgb)
+
+		p.DefaultColor.R = uint32(r)
+		p.DefaultColor.G = uint32(g)
+		p.DefaultColor.B = uint32(b)
+	}
 	p.Print(img)
 }
