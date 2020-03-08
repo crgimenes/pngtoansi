e5e0bf52f8d352be862d2d4e03db68830c6c769c
crgimenes <crg@crg.eti.br>
2020-03-08 14:46:32 -0300

    add tests

--- imgtoansi/imgtoansi_test.go
+++ imgtoansi/imgtoansi_test.go
@@ -3,6 +3,8 @@ package imgtoansi
 import (
 	"bytes"
 	"image"
+	"image/png"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -12,7 +14,10 @@ func TestNew(t *testing.T) {
 		name string
 		want *ImgToANSI
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			want: &ImgToANSI{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,7 +41,55 @@ func TestImgToANSI_SetRGB(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				rgb: "FFFFFF",
+			},
+			fields: fields{
+				DefaultColor: RGB{
+					R: 255,
+					G: 255,
+					B: 255,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "success 2",
+			args: args{
+				rgb: "FFFF",
+			},
+			fields: fields{
+				DefaultColor: RGB{
+					R: 255,
+					G: 255,
+					B: 0,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "success 3",
+			args: args{
+				rgb: "FF",
+			},
+			fields: fields{
+				DefaultColor: RGB{
+					R: 255,
+					G: 0,
+					B: 0,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "error",
+			args: args{
+				rgb: "not hexa",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -64,7 +117,45 @@ func TestImgToANSI_PrintFile(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				fileName:   "../examples/debian.png",
+				defaultRGB: "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success 2",
+			args: args{
+				fileName:   "../examples/debian.png",
+				defaultRGB: "FFFFFF",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success 3",
+			args: args{
+				fileName:   "../examples/test-01.png",
+				defaultRGB: "FFFFFF",
+			},
+			wantErr: false,
+		},
+		{
+			name: "error",
+			args: args{
+				fileName:   "../examples/test-01.png",
+				defaultRGB: "not hexa",
+			},
+			wantErr: true,
+		},
+		{
+			name: "error 2",
+			args: args{
+				fileName: "file error",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -93,7 +184,13 @@ func TestImgToANSI_FprintFile(t *testing.T) {
 		wantW   string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				fileName: "../examples/test-02.png",
+			},
+			wantW: "\x1b[48;2;0;0;0m \x1b[38;2;255;255;255m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀\n\x1b[0m\n",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -106,37 +203,24 @@ func TestImgToANSI_FprintFile(t *testing.T) {
 				return
 			}
 			if gotW := w.String(); gotW != tt.wantW {
-				t.Errorf("ImgToANSI.FprintFile() = %v, want %v", gotW, tt.wantW)
+				t.Errorf("ImgToANSI.FprintFile() = %q, want %q", gotW, tt.wantW)
 			}
-		})
-	}
-}
-
-func TestImgToANSI_Print(t *testing.T) {
-	type fields struct {
-		DefaultColor RGB
-	}
-	type args struct {
-		img image.Image
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := &ImgToANSI{
-				DefaultColor: tt.fields.DefaultColor,
-			}
-			p.Print(tt.args.img)
 		})
 	}
 }
 
 func TestImgToANSI_Fprint(t *testing.T) {
+	f, err := os.Open("../examples/test-02.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	type fields struct {
 		DefaultColor RGB
 	}
@@ -149,7 +233,27 @@ func TestImgToANSI_Fprint(t *testing.T) {
 		args   args
 		wantW  string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				img: img,
+			},
+			wantW: "\x1b[48;2;0;0;0m \x1b[38;2;255;255;255m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀\n\x1b[0m\n",
+		},
+		{
+			name: "success 2",
+			args: args{
+				img: img,
+			},
+			fields: fields{
+				DefaultColor: RGB{
+					R: 255,
+					G: 255,
+					B: 255,
+				},
+			},
+			wantW: "\x1b[48;2;0;0;0m \x1b[38;2;255;255;255m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀\n\x1b[0m\n",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
